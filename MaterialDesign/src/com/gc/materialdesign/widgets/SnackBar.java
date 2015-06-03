84f865f6a123dcceb5fe53968a47d9137c8fee80
Yicheng Zhang <zhangyicheng1234@gmail.com>
2015-06-03 17:30:06 -0400

    Fix snackbar cancel on touch outside bug.

--- MaterialDesign/src/com/gc/materialdesign/widgets/SnackBar.java
+++ MaterialDesign/src/com/gc/materialdesign/widgets/SnackBar.java
@@ -18,56 +18,62 @@ import android.widget.TextView;
 import com.gc.materialdesign.R;
 import com.gc.materialdesign.views.ButtonFlat;
 
-public class SnackBar extends Dialog{
-	
+public class SnackBar extends Dialog {
+
 	String text;
-	float textSize = 14;//Roboto Regular 14sp 
+	float textSize = 14;// Roboto Regular 14sp
 	String buttonText;
 	View.OnClickListener onClickListener;
 	Activity activity;
 	View view;
+	View rootView;
 	ButtonFlat button;
 	int backgroundSnackBar = Color.parseColor("#333333");
-	int backgroundButton = Color.parseColor("#1E88E5");
-	
+	int backgroundButton = Color.parseColor("#F44336");
+
 	OnHideListener onHideListener;
 	// Timer
 	private boolean mIndeterminate = false;
+	private boolean cancelableTouchOutside = false;
+
 	private int mTimer = 3 * 1000;
-	
+
 	// With action button
-	public SnackBar(Activity activity, String text, String buttonText, View.OnClickListener onClickListener) {
+	public SnackBar(Activity activity, String text, String buttonText,
+			View.OnClickListener onClickListener) {
 		super(activity, android.R.style.Theme_Translucent);
 		this.activity = activity;
 		this.text = text;
 		this.buttonText = buttonText;
 		this.onClickListener = onClickListener;
 	}
-	
+
 	// Only text
 	public SnackBar(Activity activity, String text) {
 		super(activity, android.R.style.Theme_Translucent);
 		this.activity = activity;
 		this.text = text;
 	}
-	
+
 	@Override
-	  protected void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    requestWindowFeature(Window.FEATURE_NO_TITLE);
-	    setContentView(R.layout.snackbar);
-	    setCanceledOnTouchOutside(false);
-	    ((TextView)findViewById(R.id.text)).setText(text); 
-	    ((TextView)findViewById(R.id.text)).setTextSize(textSize); //set textSize
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.snackbar);
+
+		((TextView) findViewById(R.id.text)).setText(text);
+		((TextView) findViewById(R.id.text)).setTextSize(textSize); // set
+																	// textSize
 		button = (ButtonFlat) findViewById(R.id.buttonflat);
-		if(text == null || onClickListener == null){
+		if (text == null || onClickListener == null) {
 			button.setVisibility(View.GONE);
-		}else{
+		}
+		else {
 			button.setText(buttonText);
 			button.setBackgroundColor(backgroundButton);
-			
+
 			button.setOnClickListener(new View.OnClickListener() {
-				
+
 				@Override
 				public void onClick(View v) {
 					dismiss();
@@ -75,71 +81,102 @@ public class SnackBar extends Dialog{
 				}
 			});
 		}
+
 		view = findViewById(R.id.snackbar);
+
 		view.setBackgroundColor(backgroundSnackBar);
+
+		rootView = findViewById(R.id.snackbar_rootView);
+		setCanceledOnTouchOutside(false);
 	}
-	
+
+	@Override
+	public void setCanceledOnTouchOutside(boolean cancel) {
+		// TODO Auto-generated method stub
+		super.setCanceledOnTouchOutside(cancel);
+
+		if (cancel) {
+			rootView.setOnTouchListener(new View.OnTouchListener() {
+
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					// TODO Auto-generated method stub
+
+					dismiss();
+
+					return false;
+				}
+			});
+		}
+		else {
+			rootView.setOnTouchListener(null);
+		}
+	}
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		return activity.dispatchTouchEvent(event);
 	}
-	
+
 	@Override
 	public void onBackPressed() {
 	}
-	
+
 	@Override
 	public void show() {
 		super.show();
 		view.setVisibility(View.VISIBLE);
-		view.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.snackbar_show_animation));
+		view.startAnimation(AnimationUtils.loadAnimation(activity,
+				R.anim.snackbar_show_animation));
 		if (!mIndeterminate) {
-		    dismissTimer.start();
+			dismissTimer.start();
 		}
 	}
-	
-	// Dismiss timer 
+
+	// Dismiss timer
 	Thread dismissTimer = new Thread(new Runnable() {
-		
+
 		@Override
 		public void run() {
 			try {
 				Thread.sleep(mTimer);
-			} catch (InterruptedException e) {
+			}
+			catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			handler.sendMessage(new Message());
 		}
 	});
-	
+
 	Handler handler = new Handler(new Handler.Callback() {
-		
+
 		@Override
 		public boolean handleMessage(Message msg) {
-			 if(onHideListener != null) {
-				 onHideListener.onHide();
-			 }
+			if (onHideListener != null) {
+				onHideListener.onHide();
+			}
 			dismiss();
 			return false;
 		}
 	});
-	
+
 	/**
 	 * @author Jack Tony
 	 */
 	@Override
 	public void dismiss() {
-		Animation anim = AnimationUtils.loadAnimation(activity, R.anim.snackbar_hide_animation);
+		Animation anim = AnimationUtils.loadAnimation(activity,
+				R.anim.snackbar_hide_animation);
 		anim.setAnimationListener(new AnimationListener() {
-			
+
 			@Override
 			public void onAnimationStart(Animation animation) {
 			}
-			
+
 			@Override
 			public void onAnimationRepeat(Animation animation) {
 			}
-			
+
 			@Override
 			public void onAnimationEnd(Animation animation) {
 				SnackBar.super.dismiss();
@@ -147,24 +184,24 @@ public class SnackBar extends Dialog{
 		});
 		view.startAnimation(anim);
 	}
-	
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		// TODO 自动生成的方法存根
-		 if (keyCode == KeyEvent.KEYCODE_BACK )  {
-			 dismiss();
-		 }
+
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			dismiss();
+		}
 		return super.onKeyDown(keyCode, event);
 	}
-	
+
 	public void setMessageTextSize(float size) {
 		textSize = size;
 	}
-	
+
 	public void setIndeterminate(boolean indeterminate) {
-        	mIndeterminate = indeterminate;
+		mIndeterminate = indeterminate;
 	}
-	
+
 	public boolean isIndeterminate() {
 		return mIndeterminate;
 	}
@@ -172,41 +209,41 @@ public class SnackBar extends Dialog{
 	public void setDismissTimer(int time) {
 		mTimer = time;
 	}
-	
+
 	public int getDismissTimer() {
 		return mTimer;
 	}
-	
+
 	/**
 	 * Change background color of SnackBar
 	 * @param color
 	 */
-	public void setBackgroundSnackBar(int color){
+	public void setBackgroundSnackBar(int color) {
 		backgroundSnackBar = color;
-		if(view != null)
+		if (view != null)
 			view.setBackgroundColor(color);
 	}
-	
+
 	/**
 	 * Chage color of FlatButton in Snackbar
 	 * @param color
 	 */
-	public void setColorButton(int color){
+	public void setColorButton(int color) {
 		backgroundButton = color;
-		if(button != null)
+		if (button != null)
 			button.setBackgroundColor(color);
 	}
-	
+
 	/**
 	 * This event start when snackbar dismish without push the button
 	 * @author Navas
 	 *
 	 */
-	public interface OnHideListener{
+	public interface OnHideListener {
 		public void onHide();
 	}
-	
-	public void setOnhideListener(OnHideListener onHideListener){
+
+	public void setOnhideListener(OnHideListener onHideListener) {
 		this.onHideListener = onHideListener;
 	}
 }
